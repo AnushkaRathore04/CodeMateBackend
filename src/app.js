8cccec58d73d7aeba2a431c0f6fd55f8b25d1b77
AnushkaRathore04 <anushkarathore0401@gmail.com>
2025-06-24 18:14:53 +0530

    exploring get ,post ,delete

--- src/app.js
+++ src/app.js
@@ -7,14 +7,41 @@
  const app = express();   //this is like creating a new server 
 
  //code for handling incoming request
+
+ //if we introduce this ("/") rout handler that it will override other rout handler 
+//  app.use("/",(req,res) => {
+//     res.send("Anushka Here! ");
+//  });
+
+//get call to get data
+app.get("/user", (req,res) =>{
+   res.send({firstNAme: "Anushka", lastName: "Rathore"});
+});
+
+//post call
+app.post("/user",(req,res) => {
+   res.send("data saved successfully to database");
+});
+
+//delete call
+app.delete("/user",(req,res) => {
+   res.send("data deleted successfully");
+});
  // ["/rest" is request handler for rest, now server will response to (localhost:3000/rest)]
  app.use("/rest",(req,res) => {
-    res.send("Hello from the server! ")
- })
+    res.send("Hello from the server! ");
+ });
 
- app.use("/hello",(req,res) => {
-    res.send("server! ")
- })
+
+
+//  app.use("/hello",(req,res) => {
+//     res.send("server! ");
+//  });
+
+//  app.use("/",(req,res) => {
+//     res.send("Anushka Here! ");
+//  });
+
  //to creating a new server we have to listen to the incoming request
  app.listen(3000, () =>{
     console.log("server is running successfully on port 3000...");
