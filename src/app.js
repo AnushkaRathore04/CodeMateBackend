27eaced8d12ebb95f1a21ba4d0feecf027b45e93
AnushkaRathore04 <anushkarathore0401@gmail.com>
2025-06-27 03:42:04 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -7,20 +7,38 @@
  const app = express();
 
 
-const { adminAuth } = require('./middlewares/auth');
+// const { adminAuth } = require('./middlewares/auth');
 
- //middleware
-app.use("/admin",adminAuth);
+//  //middleware
+// app.use("/admin",adminAuth);
 
 
- app.get("/admin/getAllData",(req,res) => {
-   //logic for checking if request is authorized
-   res.send("data sent");
- });
+//  app.get("/admin/getAllData",(req,res) => {
+//    //logic for checking if request is authorized
+//    res.send("data sent");
+//  });
 
- app.get("/admin/deleteUser",(req,res) => {
-   res.send("deleted data");
- });
+//  app.get("/admin/deleteUser",(req,res) => {
+//    res.send("deleted data");
+//  });
+
+//error handling
+
+
+app.use("/",(err,req,res,next) => {
+   if(err){
+      res.status(500).send("something went wrong");
+   }
+})
+
+app.get("/getUserData",(req,res) => {
+   try{
+      throw new Error("xys");
+      res.send("data sent");
+   }catch(err){
+       res.status(500).send("something went wrong");
+   }
+});
 
  //to creating a new server we have to listen to the incoming request
  app.listen(3000, () =>{
