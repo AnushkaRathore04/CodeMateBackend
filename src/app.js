9a18763bfe34adeffb95d9eb2bb3eca5378f60bf
AnushkaRathore04 <anushkarathore0401@gmail.com>
2025-07-22 22:14:52 +0530

    Finalized changes in app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(
    cors({
-      origin: "http://16.170.248.154",
+      origin: ["http://16.170.248.154", "http://localhost:5173"],
       credentials:true,
    })
 );
